3a6b2f81c54f5b668013544445815dc72e34f5df
madhankumarp1 <madhankumarpdckap@gmail.com>
2026-01-13 18:54:52 +0530

    Fix: Remove duplicate variable declaration

--- api/db.ts
+++ api/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const MONGODB_URI = process.env.MONGODB_URI;
+
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
