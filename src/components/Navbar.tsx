45ce9166e50df80b60102aa11599d4dd4b5dc6df
madhankumarp1 <madhankumarpdckap@gmail.com>
2026-01-13 18:25:36 +0530

    Cleanup: Remove unused logo import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, X, Phone } from 'lucide-react';
 import { cn } from '../lib/utils';
-import logo from '../assets/logo.png';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
